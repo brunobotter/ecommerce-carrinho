659dc6da283cd36a9e698f7aef0c7ac4d420ad60
brunobotter <62908563+brunobotter@users.noreply.github.com>
2024-06-28 18:47:34 -0300

    att

--- integration/sqs.go
+++ integration/sqs.go
@@ -2,8 +2,10 @@ package integration
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/brunobotter/ecommerce-carrinho/config"
@@ -12,8 +14,22 @@ import (
 func SendMessageToSQS(queueURL string, messageBody string) error {
 	logger := config.GetLogger("SQS")
 
-	// Cria uma sessão AWS
-	sess := session.Must(session.NewSession())
+	// Obter as credenciais do GitHub Secrets
+	accessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	// Criar uma sessão AWS usando as credenciais obtidas
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+		Credentials: credentials.NewStaticCredentials(
+			accessKeyID,
+			secretAccessKey,
+			"", // token (optional)
+		),
+	})
+	if err != nil {
+		return err
+	}
 
 	// Cria um serviço SQS
 	svc := sqs.New(sess)
